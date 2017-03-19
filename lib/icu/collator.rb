01a84b902e7efd05bf69f51d0fd0ddd593bb63cb
Erick Guan <fantasticfears@gmail.com>
2017-03-19 18:18:41 +0100

    3 convenient methods

--- lib/icu/collator.rb
+++ lib/icu/collator.rb
@@ -3,5 +3,17 @@ module ICU
     def sort(strings)
       strings.sort { |a, b| compare(a, b) }
     end
+
+    def greater?(str_a, str_b)
+      compare(str_a, str_b) > 0
+    end
+
+    def greater_or_equal?(str_a, str_b)
+      !(compare(str_a, str_b) < 0)
+    end
+
+    def equal?(str_a, str_b)
+      compare(str_a, str_b) == 0
+    end
   end
 end
